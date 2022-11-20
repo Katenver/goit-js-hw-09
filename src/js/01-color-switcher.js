55a53b5c60c9a2065bc9d4d581b8aff777504a5d
Katenver <8711724@ukr.net>
2022-11-20 21:40:15 +0200

    some

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
-
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  
+  const changeColorBtn = document.querySelector("[data-start]");
+  const stopChangeColorBtn = document.querySelector("[data-stop]");
+  const bodyEl = document.querySelector("body");
+  let timerId = null;
+  stopChangeColorBtn.disabled = true;
+  
+  changeColorBtn.addEventListener("click", () => {
+    bodyEl.style.background = getRandomHexColor();
+    timerId = setInterval(
+      () => (bodyEl.style.background = getRandomHexColor()),
+      1000
+    );
+    changeColorBtn.disabled = true;
+    stopChangeColorBtn.disabled = false;
+  });
+  
+  stopChangeColorBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    stopChangeColorBtn.disabled = true;
+    changeColorBtn.disabled = false;
+  });
