9feb66f5881cf786e7471a54475ea29d80e5203d
Katenver <8711724@ukr.net>
2022-11-27 10:59:05 +0200

    update 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix';
+
+const promisForm = document.querySelector('.form');
+
+promisForm.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  const { amount, delay, step } = e.target.elements;
+  // const submitButton = e.target.lastElementChild;
+  let delayTime = +delay.value;
+  // submitButton.disabled = true;
+  for (let i = 1; i <= amount.value; i += 1) {
+    createPromise(i, +delayTime)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        console.log(``);
+      });
+    delayTime += +step.value;
   }
+  e.target.reset ();
+
+  // setTimeout(() => { 
+  //   submitButton.disabled = false;
+  // }, delayTime)
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
